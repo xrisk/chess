f7ce3513fceeec6eebfd32f6846b54860a053436
Rishav Kundu <rishav.kundu98@gmail.com>
2015-12-25 02:02:09 +0530

    API v2.1

--- python/game_manager.py
+++ python/game_manager.py
@@ -11,20 +11,47 @@ db = MongoClient("mongodb://hi:hi@ds035735.mongolab.com:35735/chess")\
     .chess.gems
 
 
-@app.route('/')
-def index():
-    return "Go to /create/ to start a new game."
+def _json(d):
+    if type(d) == str:
+        return _json({"response": d})
+    elif "response" in d:
+        return json.jsonify(d)
+    else:
+        d["response"] = "OK"
+        return json.jsonify(d)
 
 
 @app.route('/create/')
 def create_game():
-    diz_gem = Game.Game()
-    db.insert_one(diz_gem.props)
-    return json.jsonify({"response": "OK", "_id": diz_gem["_id"]})
+    """Initialises a new chess game.
+
+    Returns JSON document, with structure
+    ```
+    {
+        _id: <unique game ID>
+    }
+    ```
+    """
+    record = Game.Game()
+    db.insert_one(record.props)
+    return _json({"_id": record["_id"]})
 
 
 @app.route('/join/<url>/<color>/')
 def join(url, color):
+    """Allows a player to join a game previously created by `/create/`
+
+    `url` -- _id returned by _create_
+    `color` -- the color which this UA wants to play as
+
+    Returns a JSON document with structure =>
+    ```
+    {
+        _id: <unique game ID>
+        key: authentication secret for this UA
+    }
+    ```
+    """
     record = db.find_one({"_id": url})
     if color not in ["w", "b"]:
         return json.jsonify({"response": "Bad input."})
@@ -32,30 +59,54 @@ def join(url, color):
         db.update_one({"_id": record["_id"]},
                       {"$set": {"joined.{}".format(color): True}})
 
-        return json.jsonify({"response": "OK",
-                             "key": record["secret"][color],
-                             "_id": record["_id"]})
+        return _json({"key": record["secret"][color],
+                      "_id": record["_id"]})
     else:
-        return json.jsonify({"response": "This colour is already taken."})
+        return _json("This colour is already taken.")
 
 
-@app.route('/status/<gem_id>/')
-def get_status(gem_id):
-        record = db.find_one({"_id": gem_id})
+@app.route('/status/<game_id>/')
+def get_status(game_id):
+        """Retrieves status of a chess game previously created by `/create/`
+
+        `game_id` -- unique game ID as returned by /create/
+
+        Returns a JSON document. For the structure of this document,
+        look at the `props` attribute of Game.py.
+        """
+
+        record = db.find_one({"_id": game_id})
         if record:
             del record["secret"]
-            return json.jsonify({"response": "OK", "data": record})
+            return _json({"data": record})
         else:
-            return json.jsonify({"response": "Game not found."})
+            return _json("Game not found.")
 
 
 @app.route('/api/move/<game_id>/<secret>/', methods=["POST"])
 def make_move(game_id, secret):
+    """Makes a move :)
+
+    `game_id` -- unique game ID as returned by /create/
+    `secret` -- authentication secret for this UA as returned by /join/
+
+    Requires the following POST variables:
+
+    `fen` -- the FEN string the UA currently has
+    `move` -- the move which the UA wants to play, in SAN notation
+
+    Returns a JSON document with following structure =>
+    ```
+    {
+        fen: new FEN string after executing requested move
+    }
+    ```
+    """
     for i in ["fen", "move"]:
         try:
             assert i in request.form
         except AssertionError:
-            return json.jsonify({"response": "Bad input."})
+            return _json("Bad input.")
 
     record = db.find_one({"_id": game_id})
     if record:
@@ -68,30 +119,39 @@ def make_move(game_id, secret):
                         this_game.push_san(request.form["move"])
                         db.delete_one({"_id": game_id})
                         db.insert_one(this_game)
-                        return "OK. New FEN " + str(this_game.props["fen"])
+                        return _json("OK. New FEN " + this_game.props["fen"])
                     else:
-                        return "That is not a valid move."
+                        return _json("That is not a valid move.")
                 else:
-                    return "Invalid secret. Or it's not your turn."
+                    return _json("Invalid secret. Or it's not your turn.")
             else:
-                return "You are referring to an old position, update your FEN."
+                return _json("Invalid position, update your FEN.")
         else:
-            return "Both players have not joined yet."
+            return _json("Both players have not joined yet.")
     else:
-        return "That game could not be found."
+        return _json("That game could not be found.")
 
 
-@app.route('/api/moves/<url>/')
+@app.route('/listMoves/<url>/')
 def get_valid_moves(url):
+    """ Returns valid moves in SAN notation, for current position of a game.
+        These moves will be accepted by /api/move/`
+
+        `url` -- unique game ID to return moves for
+
+        Returns a JSON document with the following structure =>
+        {
+            moves: [array of valid moves in SAN notation]
+        }
+    """
     record = db.find_one({"_id": url})
     if record:
         fen = record["fen"]
         board = chess.Board(fen)
-        return json.jsonify({"response": "OK",
-                            "moves": [x.uci() for x in board.legal_moves]})
+        return _json({"moves": [x.uci() for x in board.legal_moves]})
     else:
-        return json.jsonify({"response": "Game not found."})
+        return _json("Game not found.")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
