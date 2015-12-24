8a801bebcf13c7b4dd980cc1ba0e96862582422a
Rishav Kundu <rishav.kundu98@gmail.com>
2015-12-24 06:03:38 +0530

    Add basic version of create game.

--- python/game_manager.py
+++ python/game_manager.py
@@ -4,10 +4,15 @@ import Game
 
 def create_game(pymongo_instance):
     diz_gem = Game.Game()
-    record = pymongo_instance.insert_one(diz_gem)
-    print record
-    diz_id = record.inserted_id
-    print diz_id
+    pymongo_instance.insert_one(diz_gem)
+    return diz_gem
 
-db = pymongo.MongoClient()
-create_game(db.chez.gems)
+
+def get_gaem(pymongo_instance, url):
+    try:
+        return pymongo_instance.find_one({"url": url})
+    except:
+        print "THEFUCK"
+
+db = pymongo.MongoClient("mongodb://hi:hi@ds035735.mongolab.com:35735/chess")
+create_game(db.chess.gems)
