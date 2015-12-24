5682a2d011cd753d68618491365e685fa4d18837
Rishav Kundu <rishav.kundu98@gmail.com>
2015-12-24 07:17:24 +0530

    it works, jim!

--- python/Game.py
+++ python/Game.py
@@ -1,6 +1,7 @@
 from collections import MutableMapping
 import string
 import random
+import chess
 
 
 def random_string(l):
@@ -12,8 +13,9 @@ class Game(MutableMapping):
         self.props = {}
         self.props["url"] = random_string(6)
         self.props["secret"] = {}
-        self.props["secret"]["one"] = random_string(3)
-        self.props["secret"]["two"] = random_string(3)
+        self.props["secret"]["white"] = random_string(3)
+        self.props["secret"]["black"] = random_string(3)
+        self.props["fen"] = chess.Board().fen()
 
     def __delitem__(self):
         pass
@@ -29,3 +31,26 @@ class Game(MutableMapping):
 
     def __setitem__(self, item, value):
         self.props[item] = value
+
+    def get_fen(self):
+        return self.props["fen"]
+
+    def moves_here(self):
+        s = chess.Board(self.props["fen"]).legal_moves
+        return [x.uci() for x in list(s)]
+
+    def move_white(self, move, secret):
+        if self.props["secret"]["white"] == secret:
+            if move in self.moves_here():
+                temp = chess.Board(self.props["fen"])
+                temp.push_uci(move)
+                self.props["fen"] = temp.fen()
+                return self.props["fen"]
+
+    def move_black(self, move, secret):
+        if self.props["secret"]["black"] == secret:
+            if move in self.moves_here():
+                temp = chess.Board(self.props["fen"])
+                temp.push_sa(move)
+                self.props["fen"] = temp.fen()
+                return self.props["fen"]
