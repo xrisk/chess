22ddd716d2a2be9c3c9bb62021c035cc4b7b0ec0
Rishav Kundu <rishav.kundu98@gmail.com>
2015-12-25 05:56:37 +0530

    Add status to api.js

--- js/api.js
+++ js/api.js
@@ -1,15 +1,28 @@
-//Here is where functions for the api go, it should be self explanitory
-function stateOfGame(ID){
-	//Do stuff with the api <---???
-
-	//Return -1 for failure
-	//Return 0 for create game
-	//Return 1 for join game
-	//return 2 for game in progress or has ended
-	//
-	//
-	//DIZZY, make sure you follow the comments carefully^^^^
+function stateOfGame(ID, callback) {
+    var req = new XMLHttpRequest();
+    req.responseType = "json";
+    req.onreadystatechange = function(e) {
+        if (req.readyState == 4) {
+            if (req.response != null) {
+                var r = JSON.parse(req.response)["data"];
+                if (r["joined"]["w"] && r["joined"]["b"])
+                    callback(2);
+                else if (r["joined"]["w"] || r["joined"]["b"])
+                    callback(1);
+                else
+                    callback(0);
+            } else {
+                callback(-1);
+            }
+        }
+    }
+var url = `http://l33tchess-api.herokuapp.com/status/${ID}/`;
+console.log(url);
+req.open("GET", url);
+req.send();
 }
+
+
 function createGame(ID, Color) {
 
 	//Return 1 for success, 0 or fail
